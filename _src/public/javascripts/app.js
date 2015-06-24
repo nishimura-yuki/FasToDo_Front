97e9fd02c61e7f737db307bcac3bd97136f7a139
西村　祐樹 <nishimura_yuki@yuki.home>
2015-06-24 14:04:03 +1000

    最低限のデザイン完了

--- _src/public/javascripts/app.js
+++ _src/public/javascripts/app.js
@@ -1,5 +1,7 @@
 
-var timezone_offset = 540; //Sydney 10h = 600m
+//var timezone_offset = 540; //Tokyo  9h = 540m
+var timezone_offset = 600; //Sydney 10h = 600m
+
 var folder_data =[
     {folderid:1, foldername:"test", count: 0},
     {folderid:2, foldername:"test2", count: 40}
@@ -243,13 +245,70 @@ var MenuTitle = React.createClass({
 })
 
 var TaskAdd = React.createClass({
+    _onClickAdd: function(){
+        if(this.state.showtype != "add"){
+            this.setState({showtype: "add"});
+        } 
+        return false;
+    },
+    _onClickCancel: function(){
+        if(this.state.showtype != "show"){
+            this.setState({showtype: "show"});
+        } 
+        return false;
+    },
+    _onSave: function(){
+    
+    },
+    getInitialState: function(){
+        return { showtype: "show" };
+    },  
     render: function(){
+        var classnameNew   = "task-add-new";
+        var classnameInput = "task-add-input";
+        var classnameClose = "task-add-close";
+        var classnameInfo  = "task-add-info";
+        if(this.state.showtype != "add"){
+            classnameClose += " hidden";
+            classnameInput += " task-add-input-close";
+            classnameInfo += " task-add-info-close";
+        }else{
+            classnameNew += " hidden";
+            classnameInfo += " task-add-info-open";
+        }
         return (
             <div className="task-add">
-                <a href="#">
-                    <img src="images/add-icon.png" alt=""/>
-                    <span>add</span>
-                </a>
+                <div className={classnameNew} >
+                    <a href="#" onClick={this._onClickAdd} >
+                        <img src="images/add-icon.png" alt=""/>
+                        <span>add</span>
+                    </a>
+                </div>
+                <div className={classnameInput} >
+                    <div className={classnameClose} >
+                        <a href="#" onClick={this._onClickCancel} >
+                            <img src="images/remove-icon.png" alt=""/>
+                        </a>
+                    </div>
+                    <div className={classnameInfo} >
+                        <div className="task-add-title" > 
+                            <input className="task-title-input" type="text" name="title" />
+                        </div>
+                        <div className="task-add-date" >
+                            <input className="task-date-input" type="text" name="date" placeholder="yyyy/mm/dd" />
+                        </div>
+                        <div className="task-add-folder" >
+                            <select name="folder">
+                                <option value="ID01">選択肢1</option>
+                                <option value="ID02">選択肢2</option>
+                                <option value="ID03">選択肢3</option>
+                            </select>
+                        </div>
+                        <div className="task-add-buttons">
+                            <button className="task-save-button" type="button" name="save">Save</button>
+                        </div>            
+                    </div>
+                </div>
             </div>
         )
     }
@@ -281,28 +340,81 @@ var TaskList = React.createClass({
 });
 
 var ActiveTasks = React.createClass({
+    _onEdit: function(id){
+        console.log("edit " + id); 
+        this.setState({editing_id: id});
+    }, 
+    getInitialState: function(){
+        return { editing_id: false };
+    },   
     render: function(){
+        var _this = this;
+        var tasks = this.props.list.map(function (task) {
+            var showtype ="show";
+            if( _this.state.editing_id == task.id ){
+                showtype = "edit"; 
+            }
+            return (
+                <ActiveTask data={task} showtype={showtype}
+                    onEdit={_this._onEdit}
+
+                />
+            );
+        });
         return (
-             
-        )    
+            <div className="active-tasks">
+                {tasks}
+            </div>
+        );            
     } 
 });
 
 var ActiveTask = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            id: React.PropTypes.string.isRequired,
+            title: React.PropTypes.string.isRequired
+        }),
+        onEdit: React.PropTypes.func.isRequired,
+        onDone: React.PropTypes.func.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        onCancel: React.PropTypes.func.isRequired,
+        onDrag: React.PropTypes.func.isRequired,
+        onDelete: React.PropTypes.func.isRequired
+    },
+    _onEdit: function(){
+        console.log("parent edit");
+        this.props.onEdit(this.props.data.id);
+    },
+    _onDone: function(){
+        console.log("parent done"); 
+        this.props.onDone(this.props.data.id);
+    },
+    _onSave: function(){
+    
+    },
+    _onCancel: function(){
+    
+    },
+    _onDrag: function(){
+    
+    },
+    _onDelete: function(){
+    
+    },
     render: function(){
+        var classname = "active-task";
+        if(this.props.showtype == "edit"){
+            classname += " active-task-edit"; 
+        }else{
+            classname += " active-task-normal";
+        }
         return (
-            <div className="active-task" >
-                <ActiveTask_Buttons />
-                <ActiveTask_Drag />
-                <img class="task-drag" src="images/list-icon.png" alt="" />
-                <div class="task-info">
-                    <img class="task-check" src="images/check-icon.png" alt="" />
-                    <div class="task-title">
-                        <p>{this.props.data.title}</p>
-                        <input type="text" value="Reading a book" />
-                    </div>
-                </div>
-                <img class="task-bin" src="images/bin-icon.png" alt="" />
+            <div className={classname} >
+                <ActiveTask_Buttons showtype={this.props.showtype} />
+                <ActiveTask_Drag showtype={this.props.showtype} />
+                <ActiveTask_Info showtype={this.props.showtype} data={this.props.data} onEdit={this._onEdit} onDone={this._onDone} />
+                <ActiveTask_Bin showtype={this.props.showtype} />
             </div>
         ) 
     }
@@ -310,23 +422,87 @@ var ActiveTask = React.createClass({
 
 var ActiveTask_Buttons = React.createClass({
     render: function(){
+        var classname = "activetask-buttons";
+        if(this.props.showtype != "edit"){
+            classname += " hidden";
+        }
         return (
-            <div className="activetask-buttons">
-                <button type="button" name="save">Save</button>
-                <button type="button" name="cancel">Cancel</button>
+            <div className={classname}>
+                <button className="task-save-button" type="button" name="save">Save</button>
+                <button className="task-cancel-button" type="button" name="cancel">Cancel</button>
             </div>    
         )    
     }
 });
 
 var ActiveTask_Drag = React.createClass({
+    
     render: function(){
+        var show = "";
+        if(this.props.showtype == "edit"){
+            show = "hidden";
+        }
         return (
             <div className="activetask-drag">
-                <img src="images/list-icon.png" alt="" /> 
+                <img className={show} src="images/list-icon.png" alt="" /> 
             </div>
         ) 
     }    
+});
+
+var ActiveTask_Info = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            id: React.PropTypes.string.isRequired,
+            title: React.PropTypes.string.isRequired
+        }),
+        showtype: React.PropTypes.string.isRequired,
+        onEdit: React.PropTypes.func.isRequired,
+        onDone: React.PropTypes.func.isRequired
+    },
+
+    clickDone: function(){
+        this.props.onDone();
+    },
+    clickInfo: function(){
+        this.props.onEdit();
+    },
+    render: function(){
+        var title_class = "activetask-title activetask-title-show"; 
+        var input_class = "activetask-title activetask-title-input";
+        if(this.props.showtype == "edit"){
+            title_class += " hidden";
+        }else{
+            input_class += " hidden";
+        }
+        return (
+            <div className="activetask-info" onClick={this.clickInfo} >
+                <div className="activetask-check" onClick={this.clickDone}>
+                    <img src="images/check-icon.png" alt="" />
+                </div>
+                <div className={title_class}>
+                    <p>{this.props.data.title}</p>
+                </div>
+                <div className={input_class}>
+                    <input className="task-title-input" type="text" value={this.props.data.title} />
+                </div>
+            </div>
+        )
+    }
+});
+
+var ActiveTask_Bin = React.createClass({
+    render: function(){
+        var classname = "activetask-bin";
+        if(this.props.showtype == "edit"){
+            classname += " hidden";
+        }
+        return (
+            <div className={classname}>
+                 <img src="images/bin-icon.png" alt="" />       
+            </div>
+        )    
+    }
 });
 
 var DoneTasks = React.createClass({
@@ -351,7 +527,7 @@ var DoneTask = React.createClass({
 
 // Render
 React.render(
-    <div>
+    <div className="wrapper">
         <Header />
         <Content />
     </div> ,
